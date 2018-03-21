59f043adadf95c234c88c961bd555ebf4330216c
andresshare <andresshare@gmail.com>
2018-03-21 12:01:04 -0500

    templates && contacto

--- contacto/views.py
+++ contacto/views.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
+
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Contacto
+from.models import Contacto
 
 
 def contacto(request):
